62a76cd1ed25fa2aab4c433040ea9197608c6fd4
Hannah Chen <csyyyyywoo@gmail.com>
2026-04-16 19:20:46 -0500

    Update heart risk project cleaning steps

--- heart-risk-final-project.R
+++ heart-risk-final-project.R
@@ -10,22 +10,43 @@ library(dplyr)
 heart <- read.csv("heart-attack-risk-prediction-dataset.csv")
 
 # Look at the data
-head(heart) # shows first few rows
-dim(heart) # tells rows and columns
+head(heart) # first few rows
+dim(heart) # number of rows and columns
 names(heart) # column names
 str(heart) # variable types
 summary(heart) # quick summary
 
-# Inspect the outcome variable
+# Check the outcome variable
 table(heart$Heart.Attack.Risk..Binary.)
 prop.table(table(heart$Heart.Attack.Risk..Binary.))
 
 # Check for missing values
-colSums(is.na(heart))
-sum(!complete.cases(heart)) # number of missing values
-heart_missing <- heart[!complete.cases(heart), ]
+colSums(is.na(heart)) # missing values in each column
+sum(!complete.cases(heart)) # number of incomplete rows
+
+# Save the incomplete rows
+heart_missing <- heart[!complete.cases(heart), ] # pulls out those incomplete rows
 dim(heart_missing)
 
-heart_nomis <- na.omit(heart) # Removes every row that has at leat one missing value
-colSums(is.na(heart_nomis))
-dim(heart_nomis)
+# Check the missing-data pattern
+colSums(is.na(heart_missing)) # which columns are missing in the incomplete rows
+
+# Optional check:
+# if this gives 1, then all incomplete rows have the same missing pattern
+nrow(unique(is.na(heart_missing)))
+
+# Remove incomplete rows
+heart_nomis <- na.omit(heart) # Removes rows whith at least one missing value
+
+# Check the cleaned data
+colSums(is.na(heart_nomis)) # should all be 0
+dim(heart_nomis) # number of rows and columns after removal
+
+
+
+# Clean Gender
+table(heart_nomis$Gender, useNA = "ifany")
+
+# Turn Gender into a factor
+heart_nomis$Gender <- as.factor(heart_nomis$Gender)
+str(heart_nomis$Gender)
